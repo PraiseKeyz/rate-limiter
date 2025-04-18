725e9f8e35c3424903bb36e11f225206d847e8fc
PraiseKeyz <praiseoluwatobilobaadebayo@gmail.com>
2025-04-18 08:51:59 +0100

    Add redis connection url

--- src/utils/redisClient.ts
+++ src/utils/redisClient.ts
@@ -1,6 +1,19 @@
 import { createClient } from 'redis';
+import dotenv from 'dotenv';
 
-export const redisClient = createClient();
+dotenv.config();
+console.log(process.env.REDIS_URL)
+
+export const redisClient = createClient({
+    url : process.env.REDIS_URL,
+    socket: {
+      reconnectStrategy: retries => {
+        console.log(`Reconnecting Redis... attempt ${retries}`);
+        return Math.min(retries * 50, 2000); // Wait before retrying
+      },
+      keepAlive: 5000, // Keep socket alive
+    },
+});
 
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
 
